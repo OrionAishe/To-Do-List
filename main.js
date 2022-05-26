3833a93cbeb42dff7d6f8414fa37a76186765fcd
Orion Antonucci <wagantonucci@hotmail.com>
2022-05-26 11:46:33 -0300

    create mobile css, add task function and completed task function

--- main.js
+++ main.js
@@ -2,18 +2,29 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
 <header>
-        <h3>My Tasks</h3>
-    <nav onclick="NovaTask()">
+        <h3 class="title">My Tasks</h3>
+    <nav>
         <span class="top"></span>
         <span class="middle"></span>
         <span class="bottom"></span>
     </nav>
 </header>
 <div class="NovaTask">
+    <div class = "button">
+            <input type="text" class="text" placeholder= "Add New Task Here">
+            <button class="button1" onclick = "Adicionar1()">></button><button class="button2" onclick = "Adicionar2()">></button>
+    </div>
+</div>
+<div class = "Tasks">
     <ul class="lista">
     </ul>
+    <div class="Completed">
+        <h3>Completed</h3>
+        <ul class="TaskCompleta">
+        </ul>
+    </div>
 </div>
-<div>
-    <h3>Completed</h3>
+<div class = "Add" onclick="NovaTask()">
+    <span class = "h"></span><span class = "v"></span>
 </div>
 `
--- script.js
+++ script.js
@@ -1,4 +1,71 @@
-function NovaTask(){
-    document.querySelector("header").disabled;
-    document.querySelector("#app").classList.toggle("blur");
+let Task = "";
+let Tasks = document.querySelector(".lista");
+let texto = document.querySelector(".text");
+let TaskCompleted = [];
+let Add;
+
+if (texto != null) {
+    texto.addEventListener("keydown", (tecla) => {
+        if (tecla.key === "Enter") {
+            Adicionar1();
+        } else {
+            return;
+        }
+    }, false);
+}
+
+function NovaTask() {
+    Add = document.querySelector(".Add");
+    document.querySelector(".v").classList.toggle("vClick");
+    document.querySelector(".h").classList.toggle("vClick");
+    Add.classList.toggle("AddClick");
+    document.querySelector("header").classList.toggle("blur");
+    document.querySelector(".Completed").classList.toggle("blur");
+    document.querySelector(".NovaTask").classList.toggle("NovaTaskClick");
+    document.querySelector(".text").focus();
+    texto = document.querySelector(".text");
+    texto.addEventListener("keydown", (tecla) => {
+        if (tecla.key === "Enter") {
+            Adicionar1();
+        } else {
+            return;
+        }
+    }, false);
+}
+
+function Adicionar1() {
+    Task = document.querySelector(".text").value;
+    document.querySelector(".text").value = "";
+    Tasks = document.querySelector(".lista");
+    TaskVazia(Task, Tasks);
+    NovaTask();
+}
+
+function Adicionar2() {
+    Task = document.querySelector(".text").value;
+    document.querySelector(".text").value = "";
+    Tasks = document.querySelector(".lista");
+    TaskVazia(Task, Tasks);
+}
+
+function TaskVazia(Task, Tasks){
+    if (Task != "") {
+        let li = document.createElement("li");
+        let span = document.createElement("span");
+        li.appendChild(span);
+        li.appendChild(document.createTextNode(Task));
+        li.classList.add("Task")
+        Tasks.appendChild(li);
+        li.addEventListener('click', ()=> {TarefaCompleta(li)});
+    }
+    return;
+}
+
+function TarefaCompleta(Tarefa) {
+    Tarefa.classList.toggle("Completo");
+    if (Tarefa.parentElement.className == "TaskCompleta") {
+        document.querySelector(".lista").appendChild(Tarefa);
+    } else {
+        document.querySelector(".TaskCompleta").appendChild(Tarefa);
+    }
 }
